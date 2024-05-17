7dd2dbb664298f51ffc4341f94d105dbf7c4a84b
Luke Marshall <luke.w.marshall@student.shu.ac.uk>
2024-05-17 15:24:22 +0100

    edit error messages

--- register.php
+++ register.php
@@ -8,7 +8,6 @@
     <link rel="stylesheet" href="./includes/styles.css">
 </head>
 
-
 <?php
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -17,13 +16,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 }
 
 if (empty($_POST['username'])) {
-    $errors[] = 'Username not entered.'; // Displays error if field is empty
+    $errors[] = 'You did not enter a username.'; // Displays error if field is empty
 } else {
     $un = $dbc->real_escape_string(trim($_POST['username'])); // Adds input from 'username' field into $un variable ready to be added to database
 }
 
 if (empty($_POST['email'])) {
-    $errors[] = 'Email not entered.';
+    $errors[] = 'You did not enter an email address.';
 } else {
     $e = $dbc->real_escape_string(trim($_POST['email']));
 }
@@ -31,7 +30,7 @@ if (empty($_POST['email'])) {
 if (!empty($_POST['pass1'])) {
     if ($_POST['pass1'] != $_POST['pass2']) // checks that both password fields match if the first password field isn't empty 
     {
-        $errors[] = "Passwords do not match."; // displays error if password fields don't match
+        $errors[] = "The above passwords do not match."; // displays error if password fields don't match
     } else {
         $p = $dbc->real_escape_string(trim($_POST['pass1']));
     }
@@ -44,7 +43,7 @@ if (empty($errors)) { // If there are no errors added to the array...
     $r = $dbc->query($q); // Query is run on the database to check an email address hasn't already been used & result is stored in $r
     $rowcount = $r->num_rows; // Checks number of rows on the result of the query
     if ($rowcount != 0) { // Checks if email address has already been used
-        $errors[] = 'Email address is already registered. Please <a href="./login.php">log in</a>.'; // Prints error that email has already been used & promps user to log in
+        $errors[] = 'The above email address is already registered. Please <a href="./login.php">log in</a>.'; // Prints error that email has already been used & promps user to log in
     }
 }
 
@@ -53,7 +52,7 @@ if (empty($errors)) { // Repeat of the above code that checks for a previously u
     $r = $dbc->query($q);
     $rowcount = $r->num_rows;
     if ($rowcount != 0) {
-        $errors[] = 'Username is already registered. Please <a href="./login.php">log in</a>.';
+        $errors[] = 'The above username is already registered. Please <a href="./login.php">log in</a>.';
     }
 }
 
@@ -62,25 +61,18 @@ if (empty($errors)) {
 			  VALUES ('$un', '$e', SHA1('$p'), NOW())"; // Inserts variables into database table under the following attributes with the corresponding values. SHA1() hashes the password, NOW() returns the date and time in the format YYYY-MM-DD HH:MM:SS
     $r = $dbc->query($q); // Runs the above query
     if ($r) {
-        echo '<div class="container" style="max-width: 500px; margin-top: 80px";>
-				  <h1>Registered!</h1>
-				  <p>You are now registered.</p>
-				  <p>Please <a href="./login.php">log in</a></div>.'; // Prompts the user to log in if everything is successful 
+        echo '<div class="container">
+                <div class="content" style="color: #12214E;">
+                    <h1>You have successfully registered.</h1>
+                    <p>Please <a href="./login.php">log in</a>.</p>
+                </div>
+              </div>'; // Prompts the user to log in if everything is successful 
     }
     $dbc->close();
     exit();
 }
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($errors)) {
-    echo '<h1 style="color: red;">Error!</h1>
-		     <p>The following error(s) occurred:<br>';
-    foreach ($errors as $msg) { // Runs through each error message stored in the array (if there are any)
-        echo " - $msg<br>"; // Prints each error as a message
-    }
-    echo '<strong>Please try again.</strong></p>';
-}
 
-echo '</div>';
 
 ?>
 
@@ -113,9 +105,16 @@ echo '</div>';
                     <input type="password" name="pass2" size="20" value="<?php if (isset($_POST['pass2'])) echo $_POST['pass2']; ?>" class="form-input" placeholder="Confirm password">
                 </div>
 
-                <button type="submit" class="form-button" style="margin-top: 15px;">REGISTER</button>
+                <button type="submit" class="form-button" style="margin-top: 15px; margin-bottom: 15px;">REGISTER</button>
             </form>
-            <br>
+            <?php if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($errors)) {
+                echo '<p>The following error(s) occurred:<br>';
+                foreach ($errors as $msg) { // Runs through each error message stored in the array (if there are any)
+                    echo " - $msg<br>"; // Prints each error as a message
+                    echo "</p>";
+                }
+            }
+            ?>
         </div>
     </div>
 </body>
