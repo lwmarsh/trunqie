e45aa3d9a31305f303a65e347570c4c8378bb972
Luke Marshall <luke.w.marshall@student.shu.ac.uk>
2024-05-17 23:44:52 +0100

    create logic for "trunqing"

---
+++ trunq.php
@@ -0,0 +1,30 @@
+<?php 
+session_start();
+
+if (!isset($_SESSION['UserID'])) {
+    load();
+    exit(); 
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    require('./includes/connect_db.php');
+    $errors = array();
+}
+
+$trunqContent = $_POST['trunq_content']; // Takes input from the form on the homepage and stores it in the variable
+
+$trunqContent = htmlspecialchars($trunqContent); // Escapes special characters
+
+$q = "INSERT INTO Trunqs (UserID, TrunqContent) VALUES (?, ?)"; // "?" acts as a placeholder for actual values
+
+$pq = $dbc->prepare($q); // "(P)repared (Q)uery" -> Compiles the statement but does not execute it
+
+$pq->bind_param("is", $_SESSION['UserID'], $trunqContent); // Binds the following parameters to the statement (where the placeholder values are). "is" indicates that the first parameter is an integer (i) and the second parameter is a string (s). The two arguments are the actual values that will be inserted.
+
+$pq->execute(); 
+$pq->close(); 
+
+include_once('./login_tools.php');
+load('home.php');
+
+?>
