af9929551765e0ef105e2f22a27d94ff4d8733ce
Luke Marshall <luke.w.marshall@student.shu.ac.uk>
2024-05-17 11:55:14 +0100

    create basic landing page

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trunqer</title>
+    <link rel="stylesheet" href="./includes/styles.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="content" style="color: #12214E;">
+            <h1>Welcome to <span style="font-family: Comfortaa;">Trunqer</span>!</h1>
+            <p>Please <a href="#">log in</a> or <a href="#">register</a>.</p>
+            <div class="footer-container">
+                <footer>
+                    Trunqer is a Twitter/X clone created by <a href="#">@lwmarsh</a>.
+                </footer>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
