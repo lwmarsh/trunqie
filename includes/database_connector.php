8a9628a03d6ec871747e0e93b72a5b35c9029a30
Luke Marshall <luke.w.marshall@student.shu.ac.uk>
2024-05-22 13:54:09 +0100

    correct changes

--- includes/database_connector.php
+++ includes/database_connector.php
@@ -1,12 +1,5 @@
 <?php 
 
-require_once('./vendor/autoload.php');
-
-use Dotenv\Dotenv;
-
-$dotenv = Dotenv::createImmutable(__DIR__, '/..');
-$dotenv->load();
-
 class DatabaseConnector {
     private $host;
     private $username;
